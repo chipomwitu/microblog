f7807c8fecfc50c5f7314b7072ecf344b1629a2b
marundu <marundu@gmail.com>
2018-08-08 12:24:39 +0300

    Added template inheritance

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return 'Hello, Flask 3!'
+def index(): 
+    posts=[
+        {
+            'author': {'username': 'Marundu'}, 
+            'body': 'Discomfitting deja vu'
+        },
+        {
+            'author': {'username': 'Rufus'},
+            'body': 'Where has time gone?'
+        }
+    ]
+    user={'username': 'chipomwitu'}
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
