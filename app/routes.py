e7ee964ee30a1c197f75748ac858bb592761c246
marundu <marundu@gmail.com>
2018-08-11 20:03:09 +0300

    Improved form validation

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import flash, redirect, render_template
 from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -19,7 +20,12 @@ def index():
     
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Log In', form=form)
