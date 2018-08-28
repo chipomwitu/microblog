ec398c43073d6380367e8f3e318275465188f3c4
marundu <marundu@gmail.com>
2018-08-28 11:15:46 +0300

    Added email configs

--- config.py
+++ config.py
@@ -5,3 +5,13 @@ class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'n6b)!zl3^+4p(5sfvr2y'
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'microblog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+    # Email configuration
+    EMAIL_USE_SMTP=True
+    MAIL_SERVER=os.eviron.get('MAIL_SERVER')
+    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 465)
+    MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL=os.environ.get('MAIL_USE_SSL')
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    ADMINS=['marundurecipeapp@gmail.com']
