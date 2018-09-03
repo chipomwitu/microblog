9ea74270c7eae51b5070aaf7ca820cc353dcc63e
marundu <marundu@gmail.com>
2018-09-03 14:55:55 +0300

    Added sending errors to email

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'n6b)!zl3^+4p(5sfvr2y'
+    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 25)
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'microblog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
     # Email configuration
     EMAIL_USE_SMTP=True
-    MAIL_SERVER=os.eviron.get('MAIL_SERVER')
-    MAIL_PORT=int(os.environ.get('MAIL_PORT') or 465)
+    MAIL_SERVER=os.environ.get('MAIL_SERVER')
     MAIL_USE_TLS=os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USE_SSL=os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
