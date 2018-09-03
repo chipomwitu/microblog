dce8de72a4edc2f7233f82c24adf7c2bd73446fd
marundu <marundu@gmail.com>
2018-09-03 15:11:59 +0300

    Added logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+
+import os
 
 app=Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +21,7 @@ db=SQLAlchemy(app)
 migrate=Migrate(app, db)
 
 if not app.debug:
+    # Send errors to email
     if app.config['MAIL_SERVER']:
         auth=None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -33,5 +36,19 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+    # Log errors to a file
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler=RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
 
 from app import errors, models, routes
