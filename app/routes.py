53f4d1d39c9ec6b6e646e7a52bb93df01e598d72
marundu <marundu@gmail.com>
2018-10-01 22:15:29 +0300

    Removed dummy posts, added ability to post

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.forms import EditProfileForm, LoginForm, RegisterForm
+from app.forms import EditProfileForm, LoginForm, RegisterForm, PostForm
 from app.models import User
 
 from datetime import datetime
@@ -17,22 +17,21 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts=[
-        {
-            'author': {'username': 'Marundu'},
-            'body': 'Discomfitting deja vu'
-        },
-        {
-            'author': {'username': 'Rufus'},
-            'body': 'Where has time gone?'
-        }
-    ]
+    form=PostForm()
+    if form.validate_on_submit():
+        post=Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    
+    posts=current_user.followed_posts().all()
 
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home', form=form, posts=posts)
 
 
 @app.route('/register', methods=['GET', 'POST'])
