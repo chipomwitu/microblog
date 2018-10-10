daf6e06df7de6584ad16233335fdce6ba557d2a6
marundu <marundu@gmail.com>
2018-10-10 16:11:36 +0300

    Added pagination

--- app/routes.py
+++ app/routes.py
@@ -29,9 +29,21 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
     
-    posts=current_user.followed_posts().all()
+    page=request.args.get('page', 1, type=int)
+    posts=current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
 
-    return render_template('index.html', title='Home', form=form, posts=posts)
+    return render_template('index.html', title='Home', form=form, posts=posts.items)
+
+
+@app.route('/explore')
+@login_required
+def explore():
+    page=request.args.get('page', 1, type=int)
+    posts=Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    
+    return render_template('index.html', title='Explore', posts=posts.items)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -44,7 +56,7 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Yaaaaaay! You are now registered!')
+        flash('Yay! You are now registered!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -141,8 +153,3 @@ def unfollow(username):
     return redirect(url_for('user', username=username))
 
 
-@app.route('/explore')
-@login_required
-def explore():
-    posts=Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
--- config.py
+++ config.py
@@ -15,3 +15,6 @@ class Config(object):
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
     ADMINS=['marundurecipeapp@gmail.com']
+    
+    # Pagination
+    POSTS_PER_PAGE=3
