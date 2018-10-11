4870e4d8e66af99aefbeb6d6278d4889820c52d8
marundu <marundu@gmail.com>
2018-10-11 14:38:53 +0300

    Added pagination

--- app/routes.py
+++ app/routes.py
@@ -32,8 +32,14 @@ def index():
     page=request.args.get('page', 1, type=int)
     posts=current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
+    
+    next_url=url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    
+    prev_url=url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
 
-    return render_template('index.html', title='Home', form=form, posts=posts.items)
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/explore')
@@ -43,7 +49,13 @@ def explore():
     posts=Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
     
-    return render_template('index.html', title='Explore', posts=posts.items)
+    next_url=url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    
+    prev_url=url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/register', methods=['GET', 'POST'])
